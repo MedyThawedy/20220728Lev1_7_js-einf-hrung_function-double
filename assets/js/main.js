ac1c65f5d6d36d0c0926fd47aeb24ebe874c17c1
MD <dhmedy@googlemail.com>
2022-07-28 15:26:36 +0200

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,10 @@ function fn_double_it_up(x) {
     console.log(x);
     document.getElementById('idOutput').value = x;
 }
+
+// Arrow 
+let arrow_fn_double_it_up = (x) => {
+    x = x * 2;
+    console.log(x);
+    document.getElementById('idOutput').value = x;
+}
